d06ce6b374edc90d7abad607f704b91ed6fc5aab
cessatcyril <cessat.cyril@gmail.com>
2021-10-19 10:59:40 +0200

    implementation do java sur la class joueur

--- jeu/src/com/company/Joueur.java
+++ jeu/src/com/company/Joueur.java
@@ -10,10 +10,21 @@ public abstract class Joueur {
     protected Deplacement deplacement = new Marcher();
 
     //Constructeur par défaut
+
+    /**
+     * constructeur par defaut
+     */
     public Joueur() {
     }
 
     //Constructeur avec paramètres
+
+    /**
+     * constructeur avec parametres
+     * @param espritCombatif definit le comportement de combat
+     * @param soin definit le comportement de soin
+     * @param deplacement definit le comportement de déplacement
+     */
     public Joueur(EspritCombatif espritCombatif, Soin soin, Deplacement deplacement) {
         this.espritCombatif = espritCombatif;
         this.soin = soin;
@@ -21,28 +32,55 @@ public abstract class Joueur {
     }
 
     //Methode de déplacement de Joueur
+
+    /**
+     * Méthode de déplacement
+     */
     public void deplace() {
         deplacement.deplace();
     }
 
     //Méthode que les combattants utilisent
+
+    /**
+     * Méthode de combat
+     */
     public void combat() {
         espritCombatif.combat();
     }
 
     //Méthode de soin
+
+    /**
+     * Méthode de soin
+     */
     public void soigner() {
         soin.soigner();
     }
     //Redéfinit le comportement au combat
+
+    /**
+     * setter
+     * @param espritCombatif definit le comportement de combat
+     */
     public void setEspritCombatif(EspritCombatif espritCombatif){
         this.espritCombatif = espritCombatif;
     }
     //    Redéfinit le comportement de soin
+
+    /**
+     * setter
+     * @param soin definit le comportement de soin
+     */
     public void setSoin(Soin soin){
         this.soin = soin;
     }
     //redéfinit le comportement de déplacement
+
+    /**
+     *setter
+     * @param deplacement definit le comportement de déplacement
+     */
     public void setDeplacement(Deplacement deplacement){
         this.deplacement = deplacement;
     }
