f4193fab6aa62b417b780b8dca9a6d344dc4424e
cessatcyril <cessat.cyril@gmail.com>
2021-10-21 11:36:45 +0200

    class Message fonctionnelle (lit, encode, decode, et ecrit le fichier)

--- td_java/src/com/company/Main.java
+++ td_java/src/com/company/Main.java
@@ -1,14 +1,20 @@
 package com.company;
 
+import com.company.model.Message;
 import com.company.tool.*;
+import org.germain.tool.ManaBox;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         Transcoder test1 = new Transcoder("CFfrkowl.aDzyS:eHjsGPZgMApWvRYVmtnK!BuU IQiEXTxbqhLdNJO,'c");
         System.out.println(test1.decodeMessage("BYAPASBNBGAPASBGASBNASAFBHBGBNAHAJBNAZAFBLADBNAPASASAJAMAPADBNBJBJBJ"));
@@ -20,6 +26,14 @@ public class Main {
         Path path = Paths.get(String.valueOf(home), "encoded.txt");
         System.out.println(home);
 
+
+        //décoder
+        //Message message = new Message(true,home,home,home);
+        //message.readNwrite("encoded.txt","key.txt");
+
+        //encoder
+        Message message1 = new Message(false, home, home, home);
+        message1.readNwrite("decoded.txt", "key.txt");
 
 
     }
--- td_java/src/com/company/model/Message.java
+++ td_java/src/com/company/model/Message.java
@@ -1,11 +1,13 @@
 package com.company.model;
 
 import com.company.tool.Transcoder;
+import org.germain.tool.ManaBox;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +19,16 @@ public class Message {
     private Path msgEncodedPath;
     private Path keyPath;
     private String key;
-    private Transcoder transcoder = new Transcoder(key);
+    private Transcoder transcoder;
 
+    /**
+     * constructeur
+     *
+     * @param b  true pour fichier codé, false pour fichier non codé
+     * @param p1 chemin pour le fichier message décodé
+     * @param p2 chemin pour le fichier message codé
+     * @param p3 chemin pour le fichier de la clé
+     */
     public Message(Boolean b, Path p1, Path p2, Path p3) {
         encoded = b;
         msgClearPath = p1;
@@ -28,35 +38,132 @@ public class Message {
     }
 
 
-    public void readNwrite(String nomFichier) {
-        List<String> listeCoder = new ArrayList<>();
+    public void readNwrite(String nomFichier, String nomCle) {
+        Path cle = Path.of(String.valueOf(keyPath), nomCle);
+        Path clear = Path.of(String.valueOf(msgClearPath), "decoded.txt");
+        Path coded = Path.of(String.valueOf(msgEncodedPath), "encoded.txt");
 
-        if (encoded == true) {
-            Path path = Paths.get(String.valueOf(msgEncodedPath), "encoded.txt");
-            try {
-                msgEncoded = Files.readAllLines(path);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            for (String ligne : msgEncoded) {
-                msgClear.add(transcoder.decodeMessage(ligne));
+        if (encoded) {
+            if (Files.exists(Path.of(String.valueOf(msgEncodedPath), nomFichier))) {
+                msgClear = new ArrayList<>();
 
-                //ecrire le fichier avec le message décoder
+                //décode clé et enregistre dans key
+                cleDecoder(cle);
+
+                //initialise transcoder
+                transcoder = new Transcoder(key);
+
+                //lit le message codé et l'enregistre dans msgEncoded
+                readEncodedMessage(nomFichier);
+
+                //boucle pour décoder le msgEncoded dans msgClear.
+                for (String ligne : msgEncoded) {
+                    msgClear.add(transcoder.decodeMessage(ligne));
+                }
+
+                //écrit le message si le fichier "decoded.txt" n'existe pas
+                writeDecodedMessage(clear, msgClear);
+            } else {
+                erreurFichier();
             }
         } else {
-            Path path = Paths.get(String.valueOf(msgClearPath), "encoded.txt");
-            try {
-                msgClear = Files.readAllLines(path);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            for (String ligne : msgClear) {
-                msgEncoded.add(transcoder.codeMessage(ligne));
+            if (Files.exists(Path.of(String.valueOf(msgClearPath), nomFichier))) {
+                msgEncoded = new ArrayList<>();
 
-                //ecrire le fichier avec le message coder
+                //décode clé et enregistre dans key
+                cleDecoder(cle);
+
+                //initialise transcoder
+                transcoder = new Transcoder(key);
+
+                //lit le message non codé et l'engergistre
+                readUncodedMessage(nomFichier);
+
+                //boucle pour codé le message
+                for (String ligne : msgClear) {
+                    msgEncoded.add(transcoder.codeMessage(ligne));
+                }
+
+                //ecrit le message si le fichier "encoded.txt" n'éxiste pas
+                writeDecodedMessage(coded, msgEncoded);
+
+            } else {
+                erreurFichier();
             }
         }
     }
 
+    /**
+     * écrit un message dans un fichier.
+     * @param message liste contenant le fichier à écrire
+     * @param fichier chemin pour le fichier contenant le message a écrire
+     */
+    private void writeDecodedMessage(Path fichier, List<String> message) {
+        if (Files.exists(fichier)) {
+            System.out.println("le fichier existe déjà !!!");
+        } else {
+            for (String chaine : message
+            ) {
+                try {
+                    Files.writeString(fichier, chaine + System.lineSeparator(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * lit et enregistre le message crypter contenue dans nomFichier
+     *
+     * @param nomFichier le nom du fichier contenant le message
+     */
+    private void readEncodedMessage(String nomFichier) {
+        Path path = Paths.get(String.valueOf(msgEncodedPath), nomFichier);
+        try {
+            msgEncoded = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * lit et enregistre le message  non  crypter contenue dans nomFichier
+     *
+     * @param nomFichier le nom du fichier contenant le message
+     */
+    private void readUncodedMessage(String nomFichier) {
+        Path path = Paths.get(String.valueOf(msgClearPath), nomFichier);
+        try {
+            msgClear = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * lit le fichier contenant la clé codé, le décode et l'enregistre dans key
+     *
+     * @param cle chemin + fichier contenant la clé
+     */
+    private void cleDecoder(Path cle) {
+        List<String> listeCle = new ArrayList<>();
+        try {
+            listeCle = Files.readAllLines(cle);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //décrypter la clé et l'enregistrer dans key
+        key = listeCle.get(0);
+        key = ManaBox.decrypt(key);
+    }
+
+    /**
+     * affiche un message d'erreur si le fichier spécifier existe déjà
+     */
+    public void erreurFichier() {
+        System.out.println("Le fichier spécifié n'éxiste pas.");
+    }
 
 }
