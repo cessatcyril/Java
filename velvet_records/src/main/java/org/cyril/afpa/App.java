7073f2d81ec582bd3744a58dabf22bd8e6554907
cessatcyril <cessat.cyril@gmail.com>
2021-10-29 12:17:42 +0200

    continuation projet Velvet

--- velvet_records/src/main/java/org/cyril/afpa/App.java
+++ velvet_records/src/main/java/org/cyril/afpa/App.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -19,7 +21,9 @@ App extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/org/cyril/afpa/gui/Velvet.fxml"));
         Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
     }
 }
--- velvet_records/src/main/java/org/cyril/afpa/gui/VelvetController.java
+++ velvet_records/src/main/java/org/cyril/afpa/gui/VelvetController.java
@@ -1,4 +1,20 @@
 package org.cyril.afpa.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class VelvetController {
+    @FXML
+    public Label titreAlb, auteurAlb;
+    @FXML
+    public ImageView pochetteAlb;
+
+    public void initialize() {
+        titreAlb.setText("Dirt");
+        auteurAlb.setText("Alice in Chains");
+        Image image = new Image(String.valueOf(getClass().getResource("pictures/Dirt.jpeg")));
+        pochetteAlb.setImage(image);
+    }
 }
